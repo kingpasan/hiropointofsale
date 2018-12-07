2600bf266dfdd2b9e0f52d653b18614fd874dd35
Pasan Semage <pmsemage@gmail.com>
2018-12-08 00:16:05 +0530

    Add project files.

---
+++ Hiro Point of Sale/Main.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace Hiro_Point_of_Sale
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.menuHiro = new System.Windows.Forms.MenuStrip();
+            this.hiroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripHiro = new System.Windows.Forms.ToolStrip();
+            this.toolStripSplitButton2 = new System.Windows.Forms.ToolStripSplitButton();
+            this.toolStripSplitButton1 = new System.Windows.Forms.ToolStripSplitButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.customToolBar1 = new Hiro_Point_of_Sale.components.CustomToolBar();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.transactionsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.touchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuHiro.SuspendLayout();
+            this.toolStripHiro.SuspendLayout();
+            this.customToolBar1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuHiro
+            // 
+            this.menuHiro.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuHiro.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.hiroToolStripMenuItem,
+            this.reportsToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.touchToolStripMenuItem,
+            this.toolsToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuHiro.Location = new System.Drawing.Point(0, 0);
+            this.menuHiro.Name = "menuHiro";
+            this.menuHiro.Size = new System.Drawing.Size(782, 28);
+            this.menuHiro.TabIndex = 0;
+            this.menuHiro.Text = "menuStrip1";
+            // 
+            // hiroToolStripMenuItem
+            // 
+            this.hiroToolStripMenuItem.Name = "hiroToolStripMenuItem";
+            this.hiroToolStripMenuItem.Size = new System.Drawing.Size(50, 24);
+            this.hiroToolStripMenuItem.Text = "Hiro";
+            // 
+            // toolStripHiro
+            // 
+            this.toolStripHiro.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStripHiro.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStripHiro.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStripHiro.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripSplitButton2,
+            this.toolStripSplitButton1,
+            this.toolStripSeparator1,
+            this.toolStripButton3,
+            this.toolStripSeparator2,
+            this.toolStripButton2,
+            this.toolStripButton4,
+            this.toolStripButton1});
+            this.toolStripHiro.Location = new System.Drawing.Point(0, 28);
+            this.toolStripHiro.Name = "toolStripHiro";
+            this.toolStripHiro.Padding = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.toolStripHiro.Size = new System.Drawing.Size(782, 57);
+            this.toolStripHiro.TabIndex = 1;
+            this.toolStripHiro.Text = "toolStrip1";
+            // 
+            // toolStripSplitButton2
+            // 
+            this.toolStripSplitButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.transactionsToolStripMenuItem});
+            this.toolStripSplitButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton2.Image")));
+            this.toolStripSplitButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton2.Name = "toolStripSplitButton2";
+            this.toolStripSplitButton2.Size = new System.Drawing.Size(79, 44);
+            this.toolStripSplitButton2.Text = "Reports";
+            this.toolStripSplitButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSplitButton1
+            // 
+            this.toolStripSplitButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripSplitButton1.Image")));
+            this.toolStripSplitButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripSplitButton1.Name = "toolStripSplitButton1";
+            this.toolStripSplitButton1.Size = new System.Drawing.Size(60, 44);
+            this.toolStripSplitButton1.Text = "View";
+            this.toolStripSplitButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 47);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(65, 44);
+            this.toolStripButton3.Text = "Options";
+            this.toolStripButton3.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(66, 44);
+            this.toolStripButton2.Text = "Refunds";
+            this.toolStripButton2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(120, 44);
+            this.toolStripButton4.Text = "View Saved Bills";
+            this.toolStripButton4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(45, 44);
+            this.toolStripButton1.Text = "Help";
+            this.toolStripButton1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // customToolBar1
+            // 
+            this.customToolBar1.ColorBottom = System.Drawing.Color.DimGray;
+            this.customToolBar1.ColorTop = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.customToolBar1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.customToolBar1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.customToolBar1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripLabel2,
+            this.toolStripButton5});
+            this.customToolBar1.Location = new System.Drawing.Point(0, 85);
+            this.customToolBar1.Name = "customToolBar1";
+            this.customToolBar1.Padding = new System.Windows.Forms.Padding(10, 5, 10, 5);
+            this.customToolBar1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.customToolBar1.Size = new System.Drawing.Size(782, 37);
+            this.customToolBar1.TabIndex = 3;
+            this.customToolBar1.Text = "customToolBar1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.toolStripLabel1.ForeColor = System.Drawing.Color.White;
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(49, 24);
+            this.toolStripLabel1.Text = "User :";
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.ForeColor = System.Drawing.Color.Purple;
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(184, 24);
+            this.toolStripLabel2.Text = "Pasan Maduranga Semage";
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(84, 24);
+            this.toolStripButton5.Text = "Log out";
+            // 
+            // transactionsToolStripMenuItem
+            // 
+            this.transactionsToolStripMenuItem.Name = "transactionsToolStripMenuItem";
+            this.transactionsToolStripMenuItem.Size = new System.Drawing.Size(181, 26);
+            this.transactionsToolStripMenuItem.Text = "Transactions";
+            // 
+            // reportsToolStripMenuItem
+            // 
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // touchToolStripMenuItem
+            // 
+            this.touchToolStripMenuItem.Name = "touchToolStripMenuItem";
+            this.touchToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.touchToolStripMenuItem.Text = "Touch";
+            // 
+            // toolsToolStripMenuItem
+            // 
+            this.toolsToolStripMenuItem.Name = "toolsToolStripMenuItem";
+            this.toolsToolStripMenuItem.Size = new System.Drawing.Size(56, 24);
+            this.toolsToolStripMenuItem.Text = "Tools";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 47);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.customToolBar1);
+            this.Controls.Add(this.toolStripHiro);
+            this.Controls.Add(this.menuHiro);
+            this.MainMenuStrip = this.menuHiro;
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Main";
+            this.Text = "Hiro Point of Sale";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.menuHiro.ResumeLayout(false);
+            this.menuHiro.PerformLayout();
+            this.toolStripHiro.ResumeLayout(false);
+            this.toolStripHiro.PerformLayout();
+            this.customToolBar1.ResumeLayout(false);
+            this.customToolBar1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuHiro;
+        private System.Windows.Forms.ToolStripMenuItem hiroToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStripHiro;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton1;
+        private System.Windows.Forms.ToolStripSplitButton toolStripSplitButton2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private components.CustomToolBar customToolBar1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripMenuItem transactionsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem touchToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+    }
+}
+
